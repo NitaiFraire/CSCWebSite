d83ce75417817248b2aec04e955a3fb89d1bd32e
NitaiFraire <nitai@hotmail.com>
2020-02-06 13:10:53 -0600

    añadidas imagenes estaticas

--- CSC/CSC/local_settings.py
+++ CSC/CSC/local_settings.py
@@ -1,12 +1,32 @@
 SECRET_KEY = 'tfdsad=upfdsar8t-q%a&&&70(cqx33$(l88jvh*3c_+8l@&v&_0%w3'
-DEBUG = False
-ALLOWED_HOSTS = ['64.227.63.169', 'csc-its.com', 'www.csc-its.com']
+
+# Production
+###############
+
+# DEBUG = False
+# ALLOWED_HOSTS = ['64.227.63.169', 'csc-its.com', 'www.csc-its.com']
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'csc',
+#         'USER': 'cscdatabase',
+#         'PASSWORD': 'Arya.stark420',
+#         'HOST': 'localhost',
+# 	'PORT': '',
+#     }
+# }
+
+# Development
+#############
+DEBUG = True
+ALLOWED_HOSTS = []
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'csc',
-        'USER': 'cscdatabase',
+        'USER': 'postgres',
         'PASSWORD': 'Arya.stark420',
         'HOST': 'localhost',
 	'PORT': '',
